1cb1ce4861ad46549561804b38354435b59e6a3a
Bon Nguyen <ngbabon@gmail.com>
2019-04-23 21:40:09 -0500

    Update MockPaymentGateway.java

--- src/main/java/mum/pmp/mstore/integration/MockPaymentGateway.java
+++ src/main/java/mum/pmp/mstore/integration/MockPaymentGateway.java
@@ -9,6 +9,7 @@ public class MockPaymentGateway {
 
 	public Boolean requestPayment(CreditCard card, String orderNumber, double amount) {
 		if(!verifyCardNumber(card)) return false;
+		if(!checkBalance(amount)) return false;
 		System.out.println("PAYMENT GATEWAY: Accepted payment resquest:" + amount);
 		System.out.println("/tAmount of: " + amount);
 		System.out.println("/tNote: " + orderNumber);
@@ -31,6 +32,15 @@ public class MockPaymentGateway {
 	
 	private Boolean verifyCardExpiryDate(String expiryDate) {
 	    return expiryDate.matches("(?:0[1-9]|1[0-2])/[0-9]{2}");
+	}
+	
+	private Boolean checkBalance(double amount) {
+		if(amount <=0 ) {
+			System.out.println("PAYMENT GATEWAY: Failed to check balance");
+			return false;
+		}
+		
+		return true;
 	}
 
 }
