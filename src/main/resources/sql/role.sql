a95347dc0a74c0fb3872091bc9e8484bf861f4a0
jeancharles18 <39065976+jeancharles18@users.noreply.github.com>
2019-05-03 10:43:15 -0500

    updated role.sql

--- src/main/resources/sql/role.sql
+++ src/main/resources/sql/role.sql
@@ -4,4 +4,4 @@ insert into mstore_db.role values (1, "ADMIN");
 insert into mstore_db.role values (2, "CUSTOMER"); 
 insert into mstore_db.role values (3, "VENDOR"); 
 insert into mstore_db.role values (4, "GUEST"); 
-insert into mstore_db.role values (5, "SUPER_ADIMIN); 
+insert into mstore_db.role values (5, "SUPER_ADIMIN"); 
